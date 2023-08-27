ee68497ae7f95cf5559df8be82f26b574e16f80d
Ekansh Gupta <96657084+Ekansh25@users.noreply.github.com>
2023-08-27 04:32:29 +0000

    3-D Animation added

--- dice.js
+++ dice.js
@@ -1,36 +1,24 @@
+async function f() {
+    await new Promise(r => setTimeout(r, 2000));
+    
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+    var randomDiceImage1 = "images/dice_" + randomNumber1 + ".png"; //dice1.png - dice6.png
+    document.querySelector(".main-1").setAttribute("src", randomDiceImage1);
+
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var randomDiceImage2 = "images/dice_" + randomNumber2 + ".png";
+    document.querySelector(".main-2").setAttribute("src", randomDiceImage2);
 
 
-
-
-
-
-
-
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
-
-var randomDiceImage = "dice-" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
-
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
-
-
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-var randomImageSource2 = "images/dice-" + randomNumber2 + ".png";
-
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
-
-//If player 1 wins
-if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+    //If player 1 wins
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+    }
+    else if (randomNumber2 > randomNumber1) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+    }
+    else {
+        document.querySelector("h1").innerHTML = "Draw!";
+    }
 }
-else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-}
-else {
-    document.querySelector("h1").innerHTML = "Draw!";
-}
+f();
